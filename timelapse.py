ed46a81c945c6e1fbd0be9a1bd3efe43b196d90e
Carrie Coxwell <carrie.coxwell@gmail.com>
2021-09-05 07:36:07 -0500

    photos now saved to photos directory

--- timelapse.py
+++ timelapse.py
@@ -2,11 +2,15 @@ from picamera import PiCamera
 from time import sleep
 
 with PiCamera() as camera:
-        camera.iso = 100
-        camera.awb_mode = 'sunlight'
-        camera.meter_mode = 'matrix'
-        camera.exposure_compensation = -1
-        sleep(2)
-        for filename in camera.capture_continuous('img{counter:04d}.jpg'):
-                print('Captured %s' % filename)
-                sleep(2)
+	camera.iso = 100
+	# Wait for the automatic gain control to settle
+	sleep(2)
+	# Now fix the values
+	camera.shutter_speed = camera.exposure_speed
+	camera.exposure_mode = 'off'
+	g = camera.awb_gains
+	camera.awb_mode = 'off'
+	camera.awb_gains = g
+	for filename in camera.capture_continuous('./photos/img{counter:04d}.jpg'):
+		print('Captured %s' % filename)
+		sleep(2)
